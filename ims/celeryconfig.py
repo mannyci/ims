dc4cf2ee901d0e8906588f5f8760288b7dae54d1
Manas Maiti <39604499+mannyci@users.noreply.github.com>
2018-11-20 18:03:23 +0100

    Add schedular to celery

--- ims/celeryconfig.py
+++ ims/celeryconfig.py
@@ -1,5 +1,15 @@
+from datetime import timedelta
+
+
 BROKER_URL = 'pyamqp://localhost:5672'
 CELERY_RESULT_BACKEND = 'rpc://localhost:5672'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+CELERYBEAT_SCHEDULE = {
+    'hostcheck_scheduler': {
+        'task': 'core.tasks.host_status',
+        'schedule': timedelta(seconds=30),
+        'options': {'expires': 20,}
+	}
+}
